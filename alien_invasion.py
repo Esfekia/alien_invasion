0ae03093595188165943e907628ae9731fd5b88b
esfekia <sertac@gmail.com>
2021-12-28 05:59:54 -0500

    Added _create_fleet helper method and displayed first alien on screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -23,6 +24,9 @@ class AlienInvasion:
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
 		
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -79,6 +83,13 @@ class AlienInvasion:
 			new_bullet = Bullet(self)
 			self.bullets.add(new_bullet)
 
+	def _create_fleet(self):
+		"""Create the fleet of aliens."""
+
+		#Make an alien.
+		alien = Alien(self)
+		self.aliens.add(alien)
+
 	def _update_screen(self):
 		"""Update images on the screen and flip to the new screen."""
 		self.screen.fill(self.settings.bg_color)
@@ -87,6 +98,9 @@ class AlienInvasion:
 		#Draw the bullets
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
+
+		#Draw the aliens
+		self.aliens.draw(self.screen)
 			
 		#Make the most recently drawn screen visible.
 		pygame.display.flip()

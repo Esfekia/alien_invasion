3be329488fa2daefb2573e78dfb54bb2380c4552
Esfekia <sertac@gmail.com>
2022-01-03 16:40:27 -0500

    Added increasing speed for aliens, bullets and ship each level.

--- alien_invasion.py
+++ alien_invasion.py
@@ -184,6 +184,7 @@ class AlienInvasion:
 			#Destroy existing bullets and create new fleet.
 			self.bullets.empty()
 			self._create_fleet()
+			self.settings.increase_speed()
 
 	def _update_aliens(self):
 		"""First check if the fleet is at an edge,

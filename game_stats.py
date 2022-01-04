23dc334a53dc202b8a60546872f23c91609fa22e
Esfekia <sertac@gmail.com>
2022-01-04 17:26:37 -0500

    Added scoreboard module barebones, for scorekeeping.

--- game_stats.py
+++ game_stats.py
@@ -11,6 +11,7 @@ class GameStats:
 	def reset_stats(self):
 		"""Initialize statistics that can change during the game."""
 		self.ships_left = self.settings.ship_limit
+		self.score =0
 		self.settings.ship_speed = 0.5
 		self.settings.bullet_speed = 2.0
 		self.settings.alien_speed = 0.5
